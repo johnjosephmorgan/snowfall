b7180538ac210be47bfcb3c122713db3894f4ce4
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-05-20 12:02:28 -0400

    python3 print.

--- egs/heroico/asr/simple_v1/local/json2text.py
+++ egs/heroico/asr/simple_v1/local/json2text.py
@@ -6,7 +6,7 @@ json_decode=json.load(input_file)
 for item in json_decode:
     my_dict={}
     my_dict['text']=item.get('text')
-    print my_dict
+    print(my_dict)
 back_json=json.dumps(my_dict, output_file)
 output_file.write(back_json)
 output_file.close() 
