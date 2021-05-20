ad7ebb896caacbe138ade04690bbbfa4bdebeee0
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-05-20 12:00:56 -0400

    Fix parens.

--- egs/heroico/asr/simple_v1/local/json2text.py
+++ egs/heroico/asr/simple_v1/local/json2text.py
@@ -1,10 +1,11 @@
 import json
+
 input_file=open('exp/data/supervisions_train.json', 'r')
 output_file=open('text.json', 'w')
 json_decode=json.load(input_file)
 for item in json_decode:
     my_dict={}
-    my_dict['text']=item.get('labels').get('text'))
+    my_dict['text']=item.get('text')
     print my_dict
 back_json=json.dumps(my_dict, output_file)
 output_file.write(back_json)
