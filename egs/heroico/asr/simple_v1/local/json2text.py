b2ba99055490c6aafeb414966e1c5fa9e1f8bfac
John Joseph Morgan <johnjosephmorgan@gmail.com>
2021-05-20 12:09:18 -0400

    No dict.

--- egs/heroico/asr/simple_v1/local/json2text.py
+++ egs/heroico/asr/simple_v1/local/json2text.py
@@ -1,12 +1,8 @@
 import json
 
 input_file=open('exp/data/supervisions_train.json', 'r')
-output_file=open('text.json', 'w')
+
 json_decode=json.load(input_file)
 for item in json_decode:
-    my_dict={}
-    my_dict['text']=item.get('text')
-    print(my_dict)
-back_json=json.dumps(my_dict, output_file)
-output_file.write(back_json)
-output_file.close() 
+    t = item.get('text')
+    print(t)
